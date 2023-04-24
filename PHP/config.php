b1e004f1980d098fb7e3d5697711a58c3765a0f7
Gueram <120452309+GeramHov@users.noreply.github.com>
2023-04-24 13:50:29 +0200

    v1.01

--- PHP/config.php
+++ PHP/config.php
@@ -1,7 +1,7 @@
 <?php
 
     try {
-        $dataBase = new PDO('mysql:host=127.0.0.1; dbname=player', 'root', '');
+        $dataBase = new PDO('mysql:host=127.0.0.1; dbname=sonicowlplayer', 'root', '');
     }
     catch (PDOException $e) {
         die('An error occured while connecting to the database: ' . $e->getMessage());
